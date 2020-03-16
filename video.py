c56bc0bf992cf2249476117be5b417e0065fd9ac
grdm13 <grdmgrdm@gmail.com>
2020-03-16 17:30:12 -0500

    video.py -- works hough circles droplets3.py -- starting working

--- video.py
+++ video.py
@@ -1,1 +1,15 @@
-print('hello')
+import cv2
+import numpy as np
+
+def HoughCir():
+    image = cv2.imread('/Users/georgedamoulakis/PycharmProjects/a1/split.jpg')
+    out = image.copy()
+    grey = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    cir = cv2.HoughCircles(grey, cv2.HOUGH_GRADIENT, 1.1, 100)
+    cir = np.round(cir[0,:]).astype("int")
+    for (x,y,r) in cir:
+       cv2.circle(out, (x,y), r, (0,255,0,4))
+       cv2.rectangle(out, (x-2, y-2), (x+2, y+2), (0,255,255))
+    cv2.imshow("Final", out)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
